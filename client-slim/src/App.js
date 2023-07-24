9d9c5ede5066a758b601ce09f6c2f0be4975c7bd
Jean-Marc Prud'homme <jm.prudhomme@optable.co>
2023-07-24 08:30:47 -0400

    Added production react app to test CI flow

---
+++ client-slim/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          🔥🔥 Hello, world. 🔥🔥
+        </p>
+        <p>
+          This apps re-renders from inside a container thanks to volume mounting.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
